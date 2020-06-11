91b5be7328ccec15aeb67e5687a1b3dd8eae1cf9
nwikei <nwike.iloeje@gmail.com>
2020-06-10 21:25:55 -0500

    updated code with new initialization method; fixed problem with configuration indexing (where configurations with index>0 fail to solve

---
+++ solventx/templates.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 17 16:33:49 2020
+
+@author: splathottam
+"""
+
+config_keys = ['variable_config','environment_config']
+
+constant_variables = ['mol_frac-Nd','mol_frac-Pr','mol_frac-Ce','mol_frac-La']
